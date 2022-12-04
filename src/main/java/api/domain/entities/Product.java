327ca7b68420cb2133dcfd33215cb09bb614d3fa
Leandro Cavalcanti <leandrocavalcanti499@gmail.com>
2022-12-04 19:58:33 -0300

    ProductModel, Sql script

--- src/main/java/api/domain/entities/Product.java
+++ src/main/java/api/domain/entities/Product.java
@@ -1,25 +1,56 @@
-//package api.domain.entities;
-//
-//import lombok.Data;
-//import lombok.ToString;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import java.time.LocalDate;
-//
-//@Data
-//@ToString
-//@Entity
-//public class Product {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Integer id;
-//
-//    private String name;
-//    private Double price;
-////    private CategoryProduct category;
-//
-//}
+package api.domain.entities;
+
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@ToString
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private Double price;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updateAt;
+
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+        joinColumns = @JoinColumn(name = "product_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    Set<CategoryProductModel> categoryProductModels = new HashSet<>();
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updateAt = Instant.now();
+    }
+
+}
